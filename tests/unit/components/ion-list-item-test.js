02abed34629a5b6f70a457de1d531581ccda61ec
Alex DiLiberto <alex@alexdiliberto.com>
2015-04-04 16:33:31 -0400

    Pass the failing tests due to naming mismatches

--- tests/unit/components/ion-list-item-test.js
+++ tests/unit/components/ion-list-item-test.js
@@ -3,7 +3,7 @@ import {
   test
 } from 'ember-qunit';
 
-moduleForComponent('ionic-list-item', {
+moduleForComponent('ion-list-item', {
   // specify the other units that are required for this test
   // needs: ['component:foo', 'helper:bar']
 });
--- tests/unit/components/ion-list-test.js
+++ tests/unit/components/ion-list-test.js
@@ -3,7 +3,7 @@ import {
   test
 } from 'ember-qunit';
 
-moduleForComponent('ionic-list', {
+moduleForComponent('ion-list', {
   // specify the other units that are required for this test
   // needs: ['component:foo', 'helper:bar']
 });
