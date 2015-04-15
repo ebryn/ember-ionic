d3069cc526ae9fb60e6525bb56e602280ec9d1c7
James Fickel <jamesfickel@gmail.com>
2015-04-15 11:33:04 -0700

    item sliding works now :)

--- addon/components/ion-item.js
+++ addon/components/ion-item.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
 import layout from '../templates/components/ion-item';
 
+Ember.LinkView.reopen({
+  attributeBindings: ["style"]
+})
 
 export default Ember.Component.extend({
   layout: layout,
@@ -23,9 +26,10 @@ export default Ember.Component.extend({
   ],
 
   translateX: null,
+  originalWidth: null,
 
   transformOutput: Ember.computed('translateX', function() {
-    return `transform: translateX(${this.get('translateX')})`;
+    return `transform: translate(${this.get('translateX')}px, 0px); transition: translate 0s`.htmlSafe();
   }),
 
   optionButtons: Ember.computed(function() {
@@ -34,5 +38,64 @@ export default Ember.Component.extend({
 
   registerOptionButton(button) {
     this.get('optionButtons').pushObject(button);
+  },
+
+  didInsertElement() {
+    let {width} = this.element.getBoundingClientRect();
+    let hammer = new Hammer(this.element);
+
+    this.originalWidth = width;
+
+    hammer.on('pan panend', event => {
+      if (event.type === 'pan') {
+        this.pan(event);
+      } else if (event.type === 'panend') {
+        this.panEnd(event);
+      }
+    });
+  },
+
+  pan(event) {
+    Ember.run(() => {
+      this.slideItem(event);
+    });
+  },
+
+  panEnd(event) {
+    Ember.run(() => {
+      this.finishSlidingItem(event);
+    })
+  },
+
+  slideItem(event) {
+    let offset;
+    if (!this.originX) {
+      this.element.getElementsByClassName("item-options")[0].classList.remove("invisible");
+      offset = this.originX = event.deltaX;
+    } else {
+      offset = event.deltaX - this.originX;
+      this.originX = event.deltaX;
+    }
+    this.moveAtIndex(offset);
+  },
+
+  moveAtIndex(offset) {
+    let withinRightMax = (this.translateX + offset) <= 0;
+    let withinLeftMax = (this.translateX + offset) >= (this.originalWidth * -.70);
+    if (withinRightMax && withinLeftMax) {
+      this.set('translateX', this.translateX + offset);
+    }
+  },
+
+  finishSlidingItem(event) {
+    let optionsWidth = this.element.getElementsByClassName("item-options")[0].offsetWidth
+    if (this.translateX < -optionsWidth) {
+      this.set('translateX', -optionsWidth);
+    } else {
+      this.element.getElementsByClassName("item-options")[0].classList.add("invisible");
+      this.set('translateX', 0);
+    }
+
+    this.originX = null;
   }
 });
